3f9937faf166fa55b77c1d2d4effe832c960b617
Mauro Del Rio <mauro@crs4.it>
2020-08-05 11:50:13 +0200

    add output as dictionary

--- python/tissue_detector_inference.py
+++ python/tissue_detector_inference.py
@@ -17,12 +17,12 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
 """
 Fully connected network for tissue detection in histopathology images
 """
 
 import argparse
+import pickle
 import sys
 import numpy as np
 
@@ -32,28 +32,53 @@ from pyeddl.tensor import Tensor
 
 import tissue_detector as td
 
+
 def main(args):
     slide_fn = args.slide_fn
     level = args.level
-    
+
     t_dec = td.tissue_detector(model_fn=args.weights_fn, th=args.threshold)
-    
-    mask = t_dec.get_mask_tissue_from_slide(slide_fn, level=level, use_openslide=args.use_openslide)
-    np.save(args.output, mask)
+
+    mask = t_dec.get_mask_tissue_from_slide(slide_fn,
+                                            level=level,
+                                            use_openslide=args.use_openslide)
+    if args.more_info_output:
+        with open(args.output, 'wb') as f:
+            pickle.dump(
+                {
+                    'mask': mask,
+                    'extraction_level': args.level,
+                    'threshold': args.threshold,
+                    'input': args.slide_fn,
+                    'weights': args.weights_fn,
+                }, f)
+    else:
+        np.save(args.output, mask)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("slide_fn", metavar="INPUT_DATASET")
-    parser.add_argument("--weights_fn", type=str, metavar="MODEL FILENAME", default=30)
+    parser.add_argument("--weights_fn",
+                        type=str,
+                        metavar="MODEL FILENAME",
+                        default=30)
     parser.add_argument("--level", type=int, metavar="INT", default=4)
     parser.add_argument("--batch-size", type=int, metavar="INT", default=2**24)
-    parser.add_argument("--threshold", type=float, metavar="THRESHOLD TO CONVERT PROB TO PREDICTIONS", default=0.5)
+    parser.add_argument("--threshold",
+                        type=float,
+                        metavar="THRESHOLD TO CONVERT PROB TO PREDICTIONS",
+                        default=0.5)
     parser.add_argument("--use-openslide", action="store_true")
     parser.add_argument("--gpu", action="store_true")
+    parser.add_argument(
+        "-I",
+        help="output will be a pickled dictionary with info about the run",
+        dest="more_info_output",
+        default=False,
+        action="store_true")
     parser.add_argument("-o",
                         help="output path",
                         dest="output",
                         default="mask")
     main(parser.parse_args(sys.argv[1:]))
-    
