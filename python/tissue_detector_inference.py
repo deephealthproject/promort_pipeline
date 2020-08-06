b3bc76646d0c106c5d542685fcd17839c2820d91
ldic-tdm <ldic@tdmnode>
2020-08-06 17:43:34 +0200

    Changes to use the tissue_detector module

--- python/tissue_detector_inference.py
+++ python/tissue_detector_inference.py
@@ -30,72 +30,15 @@ import pyeddl.eddl as eddl
 import pyecvl.ecvl as ecvl
 from pyeddl.tensor import Tensor
 
-import models
-
-def read_slide(slide_fn, level=4):
-    levels = ecvl.OpenSlideGetLevels(slide_fn)
-    dims = [0, 0] + levels[level]
-    img = ecvl.OpenSlideRead(slide_fn, level, dims)
-    t = ecvl.ImageToTensor(img)
-    t_np = t.getdata()
-    s = t_np.shape
-    t_np = t_np.transpose((1,2,0)).reshape((s[1]*s[2], 3)) # Channel last and reshape
-    t_eval = Tensor.fromarray(t_np) 
-    print (t_eval.getShape())
-    return t_eval, s
-
-
-def get_mask(prob_T_l, s, th):
-    mask_np_l = []
-    for prob_T in prob_T_l:
-        output_np = prob_T.getdata()
-        pred_np = np.zeros(output_np.shape[0])
-        pred_np[output_np[:, 1]>th] = 1
-        mask_values = pred_np
-        mask_np_l.append(mask_values)
-
-    mask_values = np.vstack(mask_np_l)
-    mask = mask_values.reshape((s[1], s[2]))
-
-    return mask
-
+import tissue_detector as td
 
 def main(args):
     slide_fn = args.slide_fn
     level = args.level
-
-    ## Load model
-    net = models.tissue_detector_DNN()
-    eddl.build(
-        net,
-        eddl.rmsprop(0.00001),
-        ["soft_cross_entropy"],
-        ["categorical_accuracy"],
-        eddl.CS_GPU() if args.gpu else eddl.CS_CPU()
-    )
-
-    eddl.load(net, args.weights_fn, "bin")
-    eddl.summary(net)
     
-    ## Load Slide
-    slide_T, s = read_slide(slide_fn, level)
-
-    ## Compute tissue mask
-    #len_T = slide_T.getShape()[0]
-    #bs = args.batch_size
-    #nb = int(np.ceil((len_T / bs)))
-    #print ("n. batches: %d" % nb)
-    #output_l = []
-    #for b in range(nb):
-    #    start = bs*b
-    #    stop = bs*(b+1) if bs*(b+1) < len_T else len_T
-    #    b_T = slide_T.select(["%d:%d" % (start, stop)])
+    t_dec = td.tissue_detector(model_fn=args.weights_fn, th=args.threshold)
     
-    output_l = eddl.predict(net, [slide_T])
-    
-    print (output_l)
-    ## Binarize the output
-    mask = get_mask(output_l, s, args.threshold)
+    mask = t_dec.get_mask_tissue_from_slide(slide_fn, level=level, use_openslide=args.use_openslide)
     np.save(args.output, mask)
 
 
@@ -106,9 +49,11 @@ if __name__ == "__main__":
     parser.add_argument("--level", type=int, metavar="INT", default=4)
     parser.add_argument("--batch-size", type=int, metavar="INT", default=2**24)
     parser.add_argument("--threshold", type=float, metavar="THRESHOLD TO CONVERT PROB TO PREDICTIONS", default=0.5)
+    parser.add_argument("--use-openslide", action="store_true")
     parser.add_argument("--gpu", action="store_true")
     parser.add_argument("-o",
                         help="output path",
                         dest="output",
                         default="mask")
     main(parser.parse_args(sys.argv[1:]))
+    
