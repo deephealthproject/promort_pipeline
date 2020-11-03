7d7dbefc3e6d6fb855debc104c1c9f6299a73e72
ldic-tdm <ldic@tdmnode>
2020-11-03 15:16:53 +0100

    refactoring of the python cassandra code

--- python/promort_cassandra.py
+++ python/promort_cassandra.py
@@ -17,46 +17,62 @@ from getpass import getpass
 from tqdm import trange, tqdm
 
 import models 
+import gc
 
-def main(args):
-    num_classes = 2
-    size = [256, 256]  # size of images
-
+def get_net(in_size=[256,256], num_classes=2, lr=1e-5, augs=False, gpu=True):
+    
     ## Network definition
-    in_ = eddl.Input([3, size[0], size[1]])
+    in_ = eddl.Input([3, in_size[0], in_size[1]])
     out = models.VGG16_promort(in_, num_classes)
     net = eddl.Model([in_], [out])
     eddl.build(
         net,
-        eddl.rmsprop(args.lr),
-        #eddl.sgd(0.001, 0.9),
+        eddl.rmsprop(lr),
         ["soft_cross_entropy"],
         ["categorical_accuracy"],
-        eddl.CS_GPU([1], mem="low_mem") if args.gpu else eddl.CS_CPU()
-    
+        eddl.CS_GPU([1], mem="low_mem") if gpu else eddl.CS_CPU()
         )
+
     eddl.summary(net)
     eddl.setlogfile(net, "promort_VGG16_classification")
    
-    ## Set augmentations
-    training_augs = ecvl.SequentialAugmentationContainer([
-        #ecvl.AugResizeDim(size),
-        #ecvl.AugMirror(.5),
-        #ecvl.AugFlip(.5),
-        #ecvl.AugRotate([-45, 45])
-        #ecvl.AugAdditivePoissonNoise([0, 10]),
-        #ecvl.AugGammaContrast([0.5, 1.5]),
-        #ecvl.AugGaussianBlur([0, 0.8]),
-        #ecvl.AugCoarseDropout([0, 0.3], [0.02, 0.05], 0.5)
-    ])
+    if augs:
+        ## Set augmentations
+        training_augs = ecvl.SequentialAugmentationContainer([
+            ecvl.AugResizeDim(size),
+            ecvl.AugMirror(.5),
+            ecvl.AugFlip(.5),
+            ecvl.AugRotate([-45, 45])
+            #ecvl.AugAdditivePoissonNoise([0, 10]),
+            #ecvl.AugGammaContrast([0.5, 1.5]),
+            #ecvl.AugGaussianBlur([0, 0.8]),
+            #ecvl.AugCoarseDropout([0, 0.3], [0.02, 0.05], 0.5)
+        ])
+        
+        validation_augs = ecvl.SequentialAugmentationContainer([
+            ecvl.AugResizeDim(size),
+        ])
+        
+        dataset_augs = [training_augs, validation_augs, None]
     
-    validation_augs = ecvl.SequentialAugmentationContainer([
-        ecvl.AugResizeDim(size),
-    ])
+    else:
+        dataset_augs = [None, None, None]
+
+    return net, dataset_augs
+
+
+def main(args):
+    num_classes = 2
+    size = [256, 256]  # size of images
     
-    dataset_augs = [training_augs, validation_augs, None]
-   
-    ### Set database to read data
+    ### Get Network
+    net, dataset_augs = get_net(in_size=size, num_classes=num_classes, lr=args.lr, augs=args.augs_on, gpu=args.gpu)
+    out = net.layers[-1]
+
+    #################################
+    ### Set database to read data ###
+    #################################
+
     if not args.cassandra_pwd_fn:
         cass_pass = getpass('Insert Cassandra password: ')	
     else:
@@ -79,8 +95,9 @@ def main(args):
             cd.save_rows(args.save_rows_fn)
 
     ### Split creation
+    data_size = args.data_size
     cd.split_setup(batch_size=32, split_ratios=[8, 2],
-                   max_patches=1000, augs=dataset_augs, seed=None)
+                   max_patches=data_size, augs=dataset_augs, seed=None)
 
 
     #################
@@ -98,8 +115,6 @@ def main(args):
     num_batches_val = cd.num_batches[1]
     
     indices = list(range(args.batch_size))
-
-    pr_bl = 50 # Print losses and metrics every pr_bl batches
 
     ### Main loop across epochs
     for e in range(args.epochs):
@@ -129,30 +144,37 @@ def main(args):
             metr = net.fiterr[1]/instances
             msg = "Epoch {:d}/{:d} (batch {:d}/{:d}) - loss: {:.3f}, acc: {:.3f}".format(e + 1, args.epochs, b + 1, num_batches_tr, loss, metr)
             pbar.set_postfix_str(msg)
-
+            
+        pbar.close()
+        
         if args.save_weights:
             print("Saving weights")
             eddl.save(net, "promort_checkpoint_%s.bin" % e, "bin")
 
-        ### Evaluation on validation set
+        ### Evaluation on validation set batches
         print("Epoch %d/%d - Evaluation" % (e + 1, args.epochs), flush=True)
         
         pbar = tqdm(range(num_batches_val))
 
-        for b in pbar:
+        for b_index, b in enumerate(pbar):
             n = 0
             x, y = cd.load_batch(1)
             x.div_(255.0)
-            eddl.forward(net, [x])
-            output = eddl.getOutput(out)
-            sum_ = 0.0
+            #eddl.forward(net, [x])
+            #output = eddl.getOutput(out)
+            #net.reset()
+            net.forward([x])
+            #output = out.output
 
+            sum_ = 0.0
+            """
             for k in range(x.getShape()[0]):
                 result = output.select([str(k)])
                 target = y.select([str(k)])
                 ca = metric.value(target, result)
                 total_metric.append(ca)
                 sum_ += ca
+                
                 if args.out_dir:
                     result_a = np.array(result, copy=False)
                     target_a = np.array(target, copy=False)
@@ -169,12 +191,15 @@ def main(args):
                         current_path, d.classes_[classe], bname
                     )
                     ecvl.ImWrite(cur_path, img_t)
+            
                 n += 1
+            
             msg = "Epoch {:d}/{:d} (batch {:d}/{:d}) - acc: {:.2f} ".format(e + 1, args.epochs, b + 1, num_batches_tr, (sum_ / args.batch_size))
             pbar.set_postfix_str(msg)
-        
-        total_avg = sum(total_metric) / len(total_metric)
-        print("Total categorical accuracy: {:.2f}\n".format(total_avg))
+            """ 
+        pbar.close()
+        #total_avg = sum(total_metric) / len(total_metric)
+        #print("Total categorical accuracy: {:.2f}\n".format(total_avg))
 
 
 if __name__ == "__main__":
@@ -183,8 +208,10 @@ if __name__ == "__main__":
     parser.add_argument("--epochs", type=int, metavar="INT", default=50)
     parser.add_argument("--batch-size", type=int, metavar="INT", default=32)
     parser.add_argument("--lr", type=float, metavar="FLOAT", default=1e-5)
+    parser.add_argument("--data-size", type=int, metavar="INT", default=1000)
     parser.add_argument("--gpu", action="store_true")
-    parser.add_argument("--save_weights", action="store_true")
+    parser.add_argument("--save-weights", action="store_true")
+    parser.add_argument("--augs-on", action="store_true")
     parser.add_argument("--out-dir", metavar="DIR",
                         help="if set, save images in this directory")
     parser.add_argument("--load-rows-fn", metavar="STR",
