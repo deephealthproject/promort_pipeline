8db6d1517529c38416e67c8012459f5b4a76a12b
Francesco Versaci <francesco.versaci@gmail.com>
2020-11-26 19:48:19 +0100

    Saving masks as (smaller) png files, using openslide+pillow

--- data-preprocessing/tum_norm_masks.py
+++ data-preprocessing/tum_norm_masks.py
@@ -1,48 +1,65 @@
 from pyspark.conf import SparkConf
 from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
-import mir
+
+from PIL import Image, ImageDraw
+import numpy as np
+import openslide
+import json
 import os
+import csv
 
-def mask_kernel(args) :
-    slide, xmlroot, mask_dir, basename, lab, suf, xmlfile = args
-    if (not os.path.exists(slide)) :
-        print(f"{slide} does not exist")
-        return
-    xmldir = list(os.walk(xmlroot))[1][0]
-    xml = os.path.join(xmldir, xmlfile)
-    reader = mir.MRimage()
-    reader.open(slide)
-    reader.XML2TIFF_3(xml, mask_dir, basename, lab, suf)
-
-def mask_gen(lab, suf, xmlfile, srcdir, slidedir, maskdir) :
-    dlist = os.scandir(srcdir)
-    basenames = [e.name for e in dlist if e.is_dir()]
-    # run parallel jobs
-    pool = mp.Pool(48)
-    args = map(lambda basename : (os.path.join(slidedir, basename + '.mrxs'),
-                                  os.path.join(srcdir, basename),
-                                  maskdir, basename, lab, suf, xmlfile),
-               basenames)
-    pool.map(mask_kernel, args)
-    pool.close()
-
-def gen_all(srcdir = '/data/promort/rois.test',
-            slidedir='/data/promort/prom2/slides',
-            maskdir='/data/promort/masks.test') :
-
-
-    # generate masks for normal cores
-    mask_gen('_2', 'mask', 'cores.xml', srcdir, slidedir, maskdir)
-    # generate masks for tumoral roi's
-    mask_gen('_0', 'mask', 'focus_regions.xml', srcdir, slidedir, maskdir)
 
 ## Interactive pyspark
 # set -x PYSPARK_DRIVER_PYTHON ipython3
-# /spark/bin/pyspark --conf spark.cores.max=24 --conf spark.default.parallelism=24 --py-files mir.py --master spark://spark-master:7077
+# /spark/bin/pyspark --conf spark.cores.max=24 --conf spark.default.parallelism=24 --master spark://spark-master:7077
 #
 ## Run program
-# /spark/bin/spark-submit --conf spark.cores.max=24 --conf spark.default.parallelism=24 --py-files mir.py run.py
+# /spark/bin/spark-submit --conf spark.cores.max=24 --conf spark.default.parallelism=24 run.py
+
+# scan focus_regions or cores csv
+def scan_csv(csvdir, csvfile, filter_lab) :
+    csvpath = os.path.join(csvdir, csvfile)
+    if (not os.path.exists(csvpath)):
+        return []
+    with open(csvpath) as infile:
+        dr = csv.DictReader(infile)
+        if (csvfile=='focus_regions.csv'):
+            for row in dr :
+                fn = row['file_name']
+                ts = row['tissue_status']
+                if (ts==filter_lab):
+                    yield(fn)
+        elif (csvfile=='cores.csv'):
+            for row in dr :
+                fn = row['file_name']
+                ts = 'TUMOR' if (int(row['focus_regions_count'])>0) else 'NORMAL'
+                if (ts==filter_lab):
+                    yield(fn)
+        else:
+            raise ValueError('Only focus_regions or cores can be extracted.')
+
+def mask_kernel(args, scale=64) :
+    slide, csvroot, maskdir, basename, lab, suf, csvfile = args
+    print(slide, maskdir, basename, lab, suf)
+    if (not os.path.exists(slide)) :
+        print(f"{slide} does not exist")
+        return
+    csvdir = list(os.walk(csvroot))[1][0]
+    wsi = openslide.OpenSlide(slide)
+    sx, sy = wsi.dimensions
+    wsi.close()
+    nx, ny = round(sx/scale), round(sy/scale)
+    img = Image.new('L', (nx, ny), 0)
+    for fn in scan_csv(csvdir, csvfile, lab):
+        csvfn = os.path.join(csvdir,fn)
+        with open(csvfn) as jsonfile:
+            pol = np.array(json.load(jsonfile))
+        pol = pol/scale
+        tpol = list(map(tuple, pol))
+        ImageDraw.Draw(img).polygon(tpol, fill=1)
+    outname = os.path.join(maskdir, lab.lower(), basename + f'_{suf}.png')
+    img.save(outname)
 
 def spark_run():
     conf = SparkConf()\
@@ -55,7 +72,7 @@ def spark_run():
     srcdir = '/data/promort/rois.test'
     slidedir='/data/promort/prom2/slides'
     maskdir='/data/promort/masks.test' # must have normal and tumor subdirs
-    work = [['_2', 'cores.xml'], ['_0', 'focus_regions.xml']]
+    work = [['NORMAL', 'cores.csv'], ['TUMOR', 'focus_regions.csv']]
     suf = 'mask'
     
     dlist = os.scandir(srcdir)
@@ -64,11 +81,11 @@ def spark_run():
     # build job list
     job_list = []
     for basename in basenames:
-        for lab, xmlfile in work:
+        for lab, csvfile in work:
             slide = os.path.join(slidedir, basename + '.mrxs')
-            xmlroot = os.path.join(srcdir, basename)
-            job_list.append((slide, xmlroot, maskdir, basename, lab,
-                             suf, xmlfile))
+            csvroot = os.path.join(srcdir, basename)
+            job_list.append((slide, csvroot, maskdir, basename, lab,
+                             suf, csvfile))
     procs = sc.defaultParallelism
     rdd = sc.parallelize(job_list, numSlices=procs)
     rdd.foreach(mask_kernel)
