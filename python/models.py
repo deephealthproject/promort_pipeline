539d9d96a413ef7f12d997d6f92b5f7a409abbc5
ldic-tdm <ldic@tdmnode>
2021-03-19 00:35:01 +0100

    Added the standar VGG16 implementation

--- python/models.py
+++ python/models.py
@@ -47,6 +47,38 @@ def VGG16_promort(in_layer, num_classes, seed=1234, init=eddl.HeNormal, l2_reg=N
     return x
 
 
+def VGG16(in_layer, num_classes, seed=1234, init=eddl.HeNormal, l2_reg=None, dropout=None):
+    x = in_layer
+    x = eddl.ReLu(init(eddl.Conv(x, 64, [3, 3]), seed))
+    x = eddl.MaxPool(eddl.ReLu(init(eddl.Conv(x, 64, [3, 3]), seed)), [2, 2], [2, 2])
+    x = eddl.ReLu(init(eddl.Conv(x, 128, [3, 3]), seed))
+    x = eddl.MaxPool(eddl.ReLu(init(eddl.Conv(x, 128, [3, 3]), seed)), [2, 2], [2, 2])
+    x = eddl.ReLu(init(eddl.Conv(x, 256, [3, 3]), seed))
+    x = eddl.ReLu(init(eddl.Conv(x, 256, [3, 3]), seed))
+    x = eddl.MaxPool(eddl.ReLu(init(eddl.Conv(x, 256, [3, 3]), seed)), [2, 2], [2, 2])
+    x = eddl.ReLu(init(eddl.Conv(x, 512, [3, 3]), seed))
+    x = eddl.ReLu(init(eddl.Conv(x, 512, [3, 3]), seed))
+    x = eddl.MaxPool(eddl.ReLu(init(eddl.Conv(x, 512, [3, 3]), seed)), [2, 2], [2, 2])
+    x = eddl.ReLu(init(eddl.Conv(x, 512, [3, 3]), seed))
+    x = eddl.ReLu(init(eddl.Conv(x, 512, [3, 3]), seed))
+    x = eddl.MaxPool(eddl.ReLu(init(eddl.Conv(x, 512, [3, 3]), seed)), [2, 2], [2, 2])
+    x = eddl.Reshape(x, [-1])
+    x = eddl.Dense(x, 4096)
+    if dropout:
+        x = eddl.Dropout(x, dropout, iw=False)
+    if l2_reg:
+        x = eddl.L2(x, l2_reg)
+    x = eddl.ReLu(init(x,seed))
+    x = eddl.Dense(x, 4096)
+    if dropout:
+        x = eddl.Dropout(x, dropout, iw=False)
+    if l2_reg:
+        x = eddl.L2(x, l2_reg)
+    x = eddl.ReLu(init(x,seed))
+    x = eddl.Softmax(eddl.Dense(x, num_classes))
+    return x
+
+
 def tissue_detector_DNN():
     in_ = eddl.Input([3])
 
