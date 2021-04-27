586f83b32f2b1caee49e3346f23562141a64de1c
ldic-tdm <ldic@tdmnode>
2021-04-27 16:13:14 +0200

    chenged structure of map and reduce to keep some structures alive

--- python/envloader.py
+++ python/envloader.py
@@ -1,19 +1,77 @@
 from cassandra.auth import PlainTextAuthProvider
 from cassandra_dataset import CassandraDataset
 
+import pyecvl.ecvl as ecvl
+import pyeddl.eddl as eddl
+from pyeddl.tensor import Tensor
+
+import models
+
+
 class EnvLoader():
-    def __init__(self, inet_pass, num):
+    def __init__(self, inet_pass, num, augs_on, batch_size,
+                 max_patches, cass_row_fn, cass_datatable,
+                 net_name, size, num_classes, lr, gpus, net_init, 
+                 dropout, l2_reg):
         self.inet_pass = inet_pass
         self.num = num
         self.cd = None
-    def start(self):
-        if (self.cd is None):
-            print('####### Starting Cassandra dataset')
-            ap = PlainTextAuthProvider(username='inet',
-                                       password=self.inet_pass)
-            cd = CassandraDataset(ap, ['cassandra_db'])
-            cd.load_rows('inet_256_rows.pckl')
-            cd.init_datatable(table='imagenet.data_256')
-            cd.split_setup(batch_size=32, split_ratios=[1]*self.num,
-                           max_patches=1300000)
-            self.cd = cd
+        self.cass_row_fn = cass_row_fn
+        self.cass_datatable = cass_datatable
+        self.max_patches = max_patches
+        self.batch_size = batch_size
+        self.augs_on = augs_on
+        self.seed = None
+        self.net_name = net_name
+        self.size = size
+        self.num_classes = num_classes
+        self.lr = lr
+        self.gpus = gpus
+        self.net_init = net_init
+        self.dropout = dropout
+        self.l2_reg = l2_reg
+        self.net = None
+    def split_setup(self, seed):
+        self.seed = seed
+        self.cd.split_setup(batch_size=self.batch_size,
+                            split_ratios=[1]*self.num,
+                            max_patches=self.max_patches, seed=seed)
+    def cassandra_setup(self, seed):
+        print('####### Starting Cassandra dataset')
+        ap = PlainTextAuthProvider(username='inet',
+                                   password=self.inet_pass)
+        cd = CassandraDataset(ap, ['cassandra_db'])
+        cd.load_rows(self.cass_row_fn)
+        cd.init_datatable(table=self.cass_datatable)
+        self.cd = cd
+        self.split_setup(seed)
+    def start(self, seed):
+        # cassandra
+        if (self.cd is None): # first execution, create data structure
+            self.cassandra_setup(seed)
+        if (self.seed != seed): # seed changed, reshuffle all
+            self.split_setup(seed)
+        # eddl network
+        if (self.net is None):
+            self.net_setup()
+        
+    def net_setup(self):
+        in_ = eddl.Input([3, self.size[0], self.size[1]])
+
+        if self.net_name == 'vgg16':
+            out = models.VGG16(in_, self.num_classes, init=self.net_init, 
+                               l2_reg=self.l2_reg, dropout=self.dropout)
+        else:
+            print('model %s not available' % self.net_name)
+            sys.exit(-1)
+
+        self.net = eddl.Model([in_], [out])
+        eddl.build(
+            self.net,
+            eddl.rmsprop(self.lr),
+            ["soft_cross_entropy"],
+            ["categorical_accuracy"],
+            eddl.CS_GPU(self.gpus, mem="low_mem") if self.gpus else eddl.CS_CPU()
+            )
+
+
--- python/imagenet_spark_cass.py
+++ python/imagenet_spark_cass.py
@@ -2,6 +2,8 @@ import argparse
 from cassandra.auth import PlainTextAuthProvider
 from cassandra_dataset import CassandraDataset
 from getpass import getpass
+from envloader import EnvLoader
+
 from pyspark import StorageLevel
 from pyspark.conf import SparkConf
 from pyspark.context import SparkContext
@@ -11,19 +13,19 @@ from tqdm import trange, tqdm
 import io
 import numpy as np
 import os
+import random
 
 import pyecvl.ecvl as ecvl
 import pyeddl.eddl as eddl
 from pyeddl.tensor import Tensor
-import models
 import pickle
 
 # Run with
-# /spark/bin/spark-submit --driver-memory 16G -c spark.driver.maxResultSize=0 --py-files cassandra_dataset.py,BPH.cpython-36m-x86_64-linux-gnu.so,/home/cesco/code/tmp/inet_256_rows.pckl,/DeepHealth/git/promort_pipeline/python/models.py --conf spark.cores.max=8 imagenet_spark_cass.py --nodes 8 --init-weights-fn /data/random_init.bin
+# /spark/bin/spark-submit --driver-memory 16G -c spark.driver.maxResultSize=0 --py-files cassandra_dataset.py,envloader.py,BPH.cpython-36m-x86_64-linux-gnu.so,/home/cesco/code/tmp/inet_256_rows.pckl,/DeepHealth/git/promort_pipeline/python/models.py --conf spark.cores.max=2 imagenet_spark_cass.py --nodes 2 --init-weights-fn /data/random_init.bin --out-dir ./  --max-patches 16 --batch-size 2 --epochs 3 --sync-iterations 2
 
 
 ### Function to create 
-def get_net(net_name='vgg16', in_size=[256,256], num_classes=2, lr=1e-5, augs=False, gpus=[1], lsb=1, init=eddl.HeNormal, dropout=None, l2_reg=None):
+def get_net(net_name='vgg16', in_size=[256,256], num_classes=2, lr=1e-5, gpus=[1], init=eddl.HeNormal, dropout=None, l2_reg=None):
 
     ## Network definition
     in_ = eddl.Input([3, in_size[0], in_size[1]])
@@ -40,94 +42,67 @@ def get_net(net_name='vgg16', in_size=[256,256], num_classes=2, lr=1e-5, augs=Fa
         eddl.rmsprop(lr),
         ["soft_cross_entropy"],
         ["categorical_accuracy"],
-        #eddl.CS_GPU([1,1], mem="low_mem") if gpu else eddl.CS_CPU()
-        eddl.CS_GPU(gpus, mem="low_mem", lsb=lsb) if gpus else eddl.CS_CPU()
-        #eddl.CS_GPU(gpus, mem="low_mem") if gpus else eddl.CS_CPU()
+        eddl.CS_GPU(gpus, mem="low_mem") if gpus else eddl.CS_CPU()
         )
 
-    #eddl.summary(net)
-    #eddl.setlogfile(net, "promort_VGG16_classification")
-
-    if augs:
-        ## Set augmentations
-        training_augs = ecvl.SequentialAugmentationContainer([
-            ecvl.AugMirror(.5),
-            ecvl.AugFlip(.5),
-            ecvl.AugRotate([-10, 10])
-        ])
-
-        validation_augs = ecvl.SequentialAugmentationContainer([
-        ])
-
-        dataset_augs = [training_augs, validation_augs, None]
-
-    else:
-        dataset_augs = [None, None, None]
-
-    return net, dataset_augs
+    return net
 
 
-def train(cass_row_fn, cass_datatable, inet_pass, max_split, init_weights, epochs, lr, augs_on, gpus, lsb, batch_size, dropout,l2_reg, max_patches=1300000, seed=123):
+def set_net_weights(net, new_weights):
+    l_l = net.layers
+    for i, l in enumerate(l_l):
+        w_l = new_weights[i]
+        if w_l:
+            bias = w_l[0]
+            weights = w_l[1]
+            l.update_weights(bias, weights)
+            eddl.distributeParams(l)
+
+
+def get_num_batches(el, seed):
+    def ret(split_index):
+        ## Environment loading
+        el.value.start(seed)
+        cd = el.value.cd
+        return(cd.num_batches[split_index])
+    return(ret)
+
+
+def train(el, index_list, init_weights, sync_iterations, lr, gpus,
+          dropout, l2_reg, seed):
     def ret(split_index):
         print ('Starting train function')
         t0 = time.time()
         
-        ### Get model 
-        print ('Creating EDDL network...')
-        net_name = "vgg16"
-        num_classes = 1000
-        size = [256, 256]  # size of images
- 
-        ### Get Network
-        net_init = eddl.HeNormal
-        net, dataset_augs = get_net(net_name='vgg16', in_size=size, num_classes=num_classes, lr=lr, augs=augs_on, gpus=gpus, lsb=lsb, init=net_init, dropout=dropout, l2_reg=l2_reg)
+        ## Environment loading
+        el.value.start(seed)
+        cd = el.value.cd
+        cd.set_indexes(index_list)
+        net = el.value.net
         out = net.layers[-1]
 
         t1 = time.time()
-        print ("Time to create VGG16 %.3f" % (t1-t0))
+        print ("Time to load the Environment  %.3f" % (t1-t0))
         t0 = t1
                 
-        ### Cassandra Split Creation
-        ap = PlainTextAuthProvider(username='inet', password=inet_pass)
-        cd = CassandraDataset(ap, ['cassandra_db'], port=9042, seed=seed)
-        cd.load_rows(cass_row_fn)
-        cd.init_datatable(table=cass_datatable)
-        cd.split_setup(batch_size=batch_size, split_ratios=[1]*max_split,
-                       max_patches=max_patches, augs=dataset_augs)
-       
-        t1 = time.time()
-        print ("Time for Cassandra set up %.3f" % (t1-t0))
-        t0 = t1
-        
         ### Updating model weights 
         if isinstance(init_weights.value, str):
             eddl.load(net, str(init_weights.value))
-            #r = [[np.empty((40960,4096), dtype=np.float32)]]
             t1 = time.time()
             print ("Time to Load weights %.3f" % (t1-t0))
             t0 = t1
 
         elif init_weights.value:
-            t1 = time.time()
-            print ("Time waiting to convert np array to tensor and set parameter to the net: %.3f" % (t1-t0))
-            t0 = t1
-
             new_weights = init_weights.value 
             print (new_weights[-2][0].shape)
 
             t1 = time.time()
             print ("Receiving data from broadcast: %.3f" % (t1-t0))
             t0 = t1
-            new_weights = [[Tensor(i) for i in l] for l in new_weights]  
             
             ## updating net weights layer by layer
-            l_l = net.layers
-            for i, l in enumerate(l_l):
-                w_l = new_weights[i]
-                if w_l:
-                    bias = w_l[0]
-                    weights = w_l[1]
-                    l.update_weights(bias, weights)
+            new_weights = [[Tensor(i) for i in l] for l in new_weights]  
+            set_net_weights(net, new_weights)
 
             t1 = time.time()
             print ("Time to convert np array to tensor and set parameter to the net: %.3f" % (t1-t0))
@@ -136,49 +111,36 @@ def train(cass_row_fn, cass_datatable, inet_pass, max_split, init_weights, epoch
         ###################
         ## Training step ##
         ###################
-        num_batches_tr = cd.num_batches[split_index]
         metric_fn = eddl.getMetric("categorical_accuracy")
         loss_fn = eddl.getLoss("soft_cross_entropy")
         
-        print ("Strating Training")
-
-        loss_l = []
-        acc_l = []
-        
         ### Main loop across epochs
-        for e in range(epochs):
-
-            ### Training 
-            print("Epoch {:d}/{:d} - Training".format(e + 1, epochs),
-                  flush=True)
-            
-            cd.rewind_splits(shuffle=True)
-            eddl.reset_loss(net)
-            total_metric = []
-            total_loss = []
-            
-            ### Looping across batches of training data
-            pbar = tqdm(range(num_batches_tr))
-
-            for b_index, b in enumerate(pbar):
-                x, y = cd.load_batch(split_index)
+        ### Training 
+        if index_list[split_index] == 0: # Epoch start
+            eddl.reset_loss(net) 
         
-                x.div_(255.0)
-                tx, ty = [x], [y]
-                eddl.train_batch(net, tx, ty)
-                
-                #print bratch train results
-                loss = eddl.get_losses(net)[0]
-                metr = eddl.get_metrics(net)[0]
-                msg = "Epoch {:d}/{:d} (batch {:d}/{:d}) - loss: {:.3f}, acc: {:.3f}".format(e + 1, epochs, b + 1, num_batches_tr, loss, metr)
-                pbar.set_postfix_str(msg)
-                total_loss.append(loss)
-                total_metric.append(metr)
+        metric_l = []
+        loss_l = []
         
-            loss_l.append(np.mean(total_loss))
-            acc_l.append(np.mean(total_metric))
+        ### Looping across batches of training data
+        pbar = tqdm(range(sync_iterations))
 
-            pbar.close()
+        for b_index, b in enumerate(pbar):
+            x, y = cd.load_batch(split_index)
+    
+            x.div_(255.0)
+            tx, ty = [x], [y]
+            eddl.train_batch(net, tx, ty)
+            
+            #print bratch train results
+            loss = eddl.get_losses(net)[0]
+            metr = eddl.get_metrics(net)[0]
+            msg = "Batch {:d}/{:d}) - loss: {:.3f}, acc: {:.3f}".format(b + 1, sync_iterations, loss, metr)
+            pbar.set_postfix_str(msg)
+            loss_l.append(loss)
+            metric_l.append(metr)
+    
+        pbar.close()
 
         t1 = time.time()
         print ("Time to perform training  %.3f" % (t1-t0))
@@ -192,7 +154,7 @@ def train(cass_row_fn, cass_datatable, inet_pass, max_split, init_weights, epoch
         print ("Time to get parameters and convert them to numpy array  %.3f" % (t1-t0))
         t0 = t1
         
-        res = (r, loss_l, acc_l)
+        res = (r, sum(loss_l), sum(metric_l))
         return res
     return ret
 
@@ -205,10 +167,10 @@ def sum_weights(in0, in1):
     t0 = time.time()
     tot_weights = [[(w0[i][j]+w1[i][j]) for j, _ in enumerate(l)] for i, l in enumerate(w0)]
     
-    tot_losses = [l0[i] + l1[1] for i, _ in enumerate(l0)]
+    tot_losses = l0 + l1 
     
-    tot_metrics = [m0[i] + m1[1] for i, _ in enumerate(m0)]
-    
+    tot_metrics = m0 + m1 
+
     t1 = time.time()
     print ("Reduce function time: %.3f" % (t1-t0))
     
@@ -244,7 +206,7 @@ def run(args):
         gpus = []
 
     epochs = args.epochs
-    sync_epochs = args.sync_epochs
+    sync_iterations = args.sync_iterations
     lr = args.lr
     augs_on = args.augs_on
     lsb = 1
@@ -255,12 +217,16 @@ def run(args):
     cass_row_fn = args.cass_row_fn
     cass_datatable = args.cass_datatable
     out_dir = args.out_dir
-
+    seed = args.seed
+    net_name = "vgg16"
+    num_classes = 1000
+    size = [256, 256]  # size of images
+    net_init = eddl.HeNormal
+    
     #########################
     ### Start parallel job ##
     #########################
 
-    m_epochs = epochs // sync_epochs
     nodes = range(num)
     par_nodes = sc.parallelize(nodes, numSlices=num)
 
@@ -270,38 +236,60 @@ def run(args):
     val_acc_l = []
 
     t0 = time.time()
+    
+    el_bc = sc.broadcast(EnvLoader(inet_pass, num, augs_on,
+                                   batch_size, max_patches,
+                                   cass_row_fn, cass_datatable,
+                                   net_name, size, num_classes,
+                                   lr, gpus, net_init, 
+                                   dropout, l2_reg))
 
-    for ep in range(m_epochs):
-        init_weights_bc = sc.broadcast(init_weights)
-        #init_weights_bc = init_weights
-        weights = par_nodes\
-            .setName('Training function')\
-            .map(train(cass_row_fn, cass_datatable, inet_pass, num, init_weights_bc, sync_epochs, lr, augs_on, gpus, lsb, batch_size, dropout,l2_reg, max_patches, seed=123))
-        result = weights\
-            .setName('Reduce function')\
-            .reduce(sum_weights)
+    for ep in range(epochs):
+        tmp_loss_l = []
+        tmp_acc_l = []
+        seed = random.getrandbits(32)
+        num_batches = par_nodes\
+                .map(get_num_batches(el_bc, seed))\
+                .reduce(lambda x,y: min(x,y))
+        n_steps = num_batches // sync_iterations
+        for macro_step in range(n_steps):
+            print ("KAIOOOOOO: %d, %d, %d" % (num_batches, n_steps, macro_step))
+            init_weights_bc = sc.broadcast(init_weights)
+            index_list = [macro_step * sync_iterations * batch_size] * num
+
+            weights = par_nodes\
+                .setName('Training function')\
+                .map(train(el_bc, index_list, init_weights_bc,
+                           sync_iterations, lr, gpus, dropout, l2_reg, seed))
+            result = weights.setName('Reduce function').reduce(sum_weights)
+            
+            weights, losses, metrics = result
+             
+            # Convert list of numpy weigths to list of tensors
+            init_weights = [[i / num for i in l] for l in weights]
+                                                              
+            tmp_loss_l.append(losses / (num * sync_iterations)) # Finalizing the average and appending to training losses history
+            tmp_acc_l.append(metrics / (num * sync_iterations)) # Finalizing the average and appending to training metrics history 
+            
+            t1 = time.time()
+            print (f"Time for macro epoch {ep}: {t1-t0:.3f}")
+            t0 = t1
         
-        weights, losses, metrics = result
-         
-        # Convert list of numpy weigths to list of tensors
-        init_weights = [[i / num for i in l] for l in weights]
-        loss_l += [i / num for i in losses] # Finalizing the average and appending to training losses history
-        acc_l += [i / num for i in metrics] # Finalizing the average and appending to training metrics history 
-        
-        t1 = time.time()
-        print (f"Time for macro epoch {ep}: {t1-t0:.3f}")
-        t0 = t1
+        loss_l.append(np.mean(tmp_loss_l))
+        acc_l.append(np.mean(tmp_acc_l))
 
         if out_dir:
+            # Store loss, metrics timeseries and weights
             history = {'loss': loss_l, 'acc': acc_l, 'val_loss': val_loss_l, 'val_acc': val_acc_l}
             pickle.dump(history, open(os.path.join(out_dir, 'history.pickle'), 'wb'))
+            # Store weights
 
 
 if __name__ == "__main__":        
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--nodes", type=int, metavar="INT", default=3, help='Number of nodes')
     parser.add_argument("--epochs", type=int, metavar="INT", default=10, help='Number of total epochs')
-    parser.add_argument("--sync-epochs", type=int, metavar="INT", default=5, help='Number of epochs between weights sync')
+    parser.add_argument("--sync-iterations", type=int, metavar="INT", default=5, help='Number of step between weights sync')
     parser.add_argument("--max-patches", type=int, metavar="INT", default=1300000, help='Number of patches to use for all splits')
     parser.add_argument("--patience", type=int, metavar="INT", default=20, help='Number of epochs after which the training is stopped if validation accuracy does not improve (delta=0.001)')
     parser.add_argument("--batch-size", type=int, metavar="INT", default=32, help='Batch size')
