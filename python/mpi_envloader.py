9f265afa41be498688cc51e134218ff94bd734eb
ldic-tdm <ldic@tdmnode>
2021-08-07 20:56:27 +0200

    Added the capabiliy of generate validation splits. Moreover the build function now can use the right optimizer and the right GPUs depending on the number of workers

--- python/mpi_envloader.py
+++ python/mpi_envloader.py
@@ -8,14 +8,17 @@ from pyeddl.tensor import Tensor
 import models
 
 from OPT_MPI import mpi_env, sgd_mpi
+import subprocess
 
 class EnvLoader():
-    def __init__(self, inet_pass, n_sync, augs_on, batch_size,
+    def __init__(self, inet_pass, n_sync, val_ratio, test_ratio, augs_on, batch_size,
                  max_patches, cass_row_fn, cass_datatable,
                  net_name, size, num_classes, lr, gpus, net_init,
-                 dropout, l2_reg):
+                 dropout, l2_reg, seed):
         
         self.MP = mpi_env(n_sync, bl=512)
+        self.val_ratio = val_ratio
+        self.test_ratio = test_ratio
         self.num = self.MP.mpi_size
         self.inet_pass = inet_pass
         self.cd = None
@@ -24,7 +27,7 @@ class EnvLoader():
         self.max_patches = max_patches
         self.batch_size = batch_size
         self.augs_on = augs_on
-        self.seed = None
+        self.seed = seed
         self.net_name = net_name
         self.size = size
         self.num_classes = num_classes
@@ -34,18 +37,32 @@ class EnvLoader():
         self.dropout = dropout
         self.l2_reg = l2_reg
         self.net = None
+        self.ngpus = 0
+        if gpus:
+            # Discover how many gpus can be used
+            result = subprocess.run(['nvidia-smi', '-L'], stdout=subprocess.PIPE)
+            self.ngpus = len(result.stdout.decode('utf-8').split('\n'))-1
 
     def split_setup(self, seed):
         self.seed = seed
+        tr_ratio = [(1 - self.val_ratio - self.test_ratio) / self.num] * self.num
+        val_ratio = [self.val_ratio / self.num] * self.num 
+        test_ratio = [self.test_ratio / self.num] * self.num 
+        split_ratios = tr_ratio + val_ratio + test_ratio
         self.cd.split_setup(batch_size=self.batch_size,
-                            split_ratios=[1] * self.num,
+                            split_ratios=split_ratios,
                             max_patches=self.max_patches, seed=seed)
-
+                            #max_patches=8000, seed=seed)
+        if self.MP.mpi_rank == 0:
+            print ("SPLIT RATIOS: %r" % split_ratios)
+            self.cd.save_splits('/tmp/curr_split.pckl')
+    
     def cassandra_setup(self, seed):
         print('####### Starting Cassandra dataset')
+        print('Seed = %r ' % seed)
         ap = PlainTextAuthProvider(username='inet',
                                    password=self.inet_pass)
-        cd = CassandraDataset(ap, ['cassandra-db'])
+        cd = CassandraDataset(ap, ['cassandra-db'], seed=seed)
         cd.load_rows(self.cass_row_fn)
         cd.init_datatable(table=self.cass_datatable)
         self.cd = cd
@@ -56,6 +73,7 @@ class EnvLoader():
         if (self.cd is None):  # first execution, create data structure
             self.cassandra_setup(seed)
         if (self.seed != seed):  # seed changed, reshuffle all
+            print ("Split setup")
             self.split_setup(seed)
         # eddl network
         if (self.net is None):
@@ -83,21 +101,29 @@ class EnvLoader():
             raise ValueError('model %s not available' % self.net_name)
         # build network
         self.net = eddl.Model([in_], [out])
-        
-        if self.MP.mpi_rank == 0:
-            gpu_mask = [1, 0]
-        else:
-            gpu_mask = [0, 1]
+       
+        if self.num == 1:
+            ## Single node execution
+                
+            eddl.build(
+                self.net,
+                eddl.sgd(self.lr),
+                ["soft_cross_entropy"],
+                ["categorical_accuracy"],
+                eddl.CS_GPU(self.gpus, mem="full_mem", lsb=1) if self.gpus else eddl.CS_CPU()
+                )
 
-        
-        eddl.build(
-            self.net,
-            sgd_mpi(self.MP, self.lr),
-            #eddl.sgd(self.lr),
-            ["soft_cross_entropy"],
-            ["categorical_accuracy"],
-            #eddl.CS_GPU(self.gpus, mem="low_mem", lsb=1) if self.gpus else eddl.CS_CPU()
-            eddl.CS_GPU(gpu_mask, mem="low_mem", lsb=1) if self.gpus else eddl.CS_CPU()
-            )
+        else:
+            ## multinode and/or multigpu
+            gpu_mask = [0] * self.ngpus
+            gpu_mask[self.MP.mpi_rank] = 1
+            
+            eddl.build(
+                self.net,
+                sgd_mpi(self.MP, self.lr),
+                ["soft_cross_entropy"],
+                ["categorical_accuracy"],
+                eddl.CS_GPU(gpu_mask, mem="full_mem", lsb=1) if self.gpus else eddl.CS_CPU()
+                )
 
         eddl.summary(self.net)
