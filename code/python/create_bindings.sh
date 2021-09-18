6f02b00d2d6836073e471fb7cbe39077c2b8dfc4
ldic-tdm <ldic@tdmnode>
2021-09-18 19:23:59 +0200

    Added the script to create sgd_mpi python bindings

---
+++ code/python/create_bindings.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/sgd_mpi/code/opt_mpi/pybind
+make
+python3 setup.py build
+ln -s /home/sgd_mpi/code/opt_mpi/pybind/build/lib.linux-x86_64-3.6/pyopt_mpi/OPT_MPI.cpython-36m-x86_64-linux-gnu.so  /home/sgd_mpi/code/python/OPT_MPI.cpython-36m-x86_64-linux-gnu.so
