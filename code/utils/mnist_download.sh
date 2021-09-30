843de1a3b7a5b598a109a1e0f911296195023507
ldic-tdm <ldic@tdmnode>
2021-09-30 22:27:07 +0200

    Added code to download mnist dataset during the docker image creation

---
+++ code/utils/mnist_download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DEST=/home/sgd_mpi/data
+mkdir -p $DEST && cd $DEST
+wget -q --show-progress https://www.dropbox.com/s/khrb3th2z6owd9t/mnist_trX.bin
+wget -q --show-progress https://www.dropbox.com/s/m82hmmrg46kcugp/mnist_trY.bin
+wget -q --show-progress https://www.dropbox.com/s/7psutd4m4wna2d5/mnist_tsX.bin
+wget -q --show-progress https://www.dropbox.com/s/q0tnbjvaenb4tjs/mnist_tsY.bin
