a76c64677b52757c8883e8e62ac75ef8806c082c
ldic-tdm <ldic@tdmnode>
2022-02-16 20:39:23 +0100

    added the label_map and the new balance features to the split creation app

--- python/cassandra_utils/cassandra_row_split_creation.py
+++ python/cassandra_utils/cassandra_row_split_creation.py
@@ -16,7 +16,7 @@ import pickle
 
 def main(args):
     print ("Requested Splits from:")
-    print ("table: %s" % args.table)
+    print ("data_table: %s" % args.data_table)
     print ("ids_table: %s" % args.ids_table)
     print ("metatable: %s" % args.metatable)
     print ("splits: %r" % args.split_ratios)
@@ -40,11 +40,25 @@ def main(args):
 
     grouping_cols = ['sample_name']
 
+    if args.lab_map:
+        lab_map = [int(i) for i in args.lab_map]
+    else:
+        lab_map = []
+
+    if args.balance:
+        if len(args.balance) != args.num_classes:
+            print ("\nError: The number of balance elements must be equal to num_classes")
+            sys.exit(-1)
+        balance = [float(i) for i in args.balance]
+    else:
+        balance = None
+    
     cd.init_listmanager(table='promort.' + args.ids_table, 
                         metatable='promort.' + args.metatable, 
                         id_col='patch_id',
                         num_classes=args.num_classes, 
                         label_col=args.label,
+                        label_map = lab_map,
                         grouping_cols=args.grouping_cols)
 
     if args.db_rows_fn:
@@ -61,14 +75,15 @@ def main(args):
     
     clm = cd._clm
 
-    data_size = min(cd._clm.tot, args.data_size)
-    if args.balanced:
-        min_class = np.min(clm._stats.sum(axis=0))
-        data_size = min(data_size, min_class*clm.num_classes)
-        print (min_class)
+    data_size = args.data_size
+    
+    #if args.balanced:
+    #    min_class = np.min(clm._stats.sum(axis=0))
+    #    data_size = min(data_size, min_class*clm.num_classes)
+    #    print (clm._stats.sum(axis=0))
+    #    print (min_class)
    
     print (clm._stats.sum(axis=0), clm.tot)
-    print ("data size: %d" % data_size)
     
     ### Check for bags
     if args.bags_pckl:
@@ -78,10 +93,15 @@ def main(args):
         bags = None
 
     ## Create the split and save it
-    cd.init_datatable(table='promort.' + args.table)
+    cd.init_datatable(table='promort.' + args.data_table)
     cd.split_setup(batch_size=args.batch_size, split_ratios=args.split_ratios,
-                                 max_patches=data_size, augs=[], bags=bags)
+                                 max_patches=data_size, augs=[], bags=bags, balance=balance)
 
+    print (clm._stats.sum(axis=0), clm.tot)
+    print (clm._split_stats)
+    print (clm._split_stats_actual)
+
+    
     out_fn = os.path.join(args.out_dir, '%s.pckl' % args.out_name)
     print ("Saving splits in: %s" % out_fn)
     cd.save_splits(out_fn)
@@ -89,7 +109,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("--table", default="data_cosk_0")
+    parser.add_argument("--data-table", default="data_cosk_0")
     parser.add_argument("--ids-table", default="ids_cosk_0")
     parser.add_argument("--metatable", default="metatable_cosk_0")
     parser.add_argument("--bags-pckl", default=None)
@@ -98,8 +118,9 @@ if __name__ == "__main__":
     parser.add_argument("--num-classes", type=int, metavar="INT", default=2)
     parser.add_argument("--batch-size", type=int, metavar="INT", default=32)
     parser.add_argument("--split-ratios", nargs='+', type=int, default="7 2 1")
-    parser.add_argument("--data-size", type=int, metavar="INT", default=100000)
-    parser.add_argument("--balanced", action="store_true", help='returns balanced splits')
+    parser.add_argument("--data-size", type=int, metavar="INT", default=None, help='Specifies the total number of patches to be used to create splits. If not specified all patches are used')
+    parser.add_argument("--lab-map", nargs='+', default = [], help='Specifies label mapping. It is used to group original dataset lebel to new class labels. For example: if the original dataset has [0, 1, 2, 3] classes using lab-map 0 0 1 1 maps the new label 0 to the old 0,1 classes and the new label 1 to the old 2,3 classes ')
+    parser.add_argument("--balance", nargs='+', default = [], help='Specifies balance among classes. The number of values must be equal to the number of classes. For example: if the dataset has three classes, using --balance 1 1 1 set the same number of patches for all classes.')
     parser.add_argument("--out-dir", metavar="DIR", default='/tmp',
                         help="destination folder")
     parser.add_argument("--out-name", metavar="DIR", default = "splits.pckl",
